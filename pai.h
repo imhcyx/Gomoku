003ab4cc63f7804958f3bd52ddaf242d1b5ce849
imhcyx <3443722+imhcyx@users.noreply.github.com>
2017-12-08 10:43:07 +0800

    adjust comment format

--- pai.h
+++ pai.h
@@ -34,7 +34,16 @@
 /* Player callback */
 
 /* The callback function should be defined like this:
- * int callback(int role, int action, pos *lastpos, pos *newpos, board_t *board, void *userdata);
+ * 
+ * int callback(
+ *    int role,
+ *    int action,
+ *    pos *lastpos,
+ *    pos *newpos,
+ *    board_t *board,
+ *    void *userdata
+ *    );
+ * 
  * role: role of self
  * action: the most recent action
  * lastpos: the position recently affected
@@ -52,21 +61,24 @@
  *  All cleanup work must be done. lastpos, newpos, board are unused.
  *
  * ACTION_NONE:
- *  Received when no previous action is avaliable. Cannot be used as return value of callback.
- *  lastpos is unused.
+ *  Received when no previous action is avaliable. Cannot be used as
+ *  return value of callback. lastpos is unused.
  *
  * ACTION_GIVEUP:
  *  Callback function should return this when the player gives up.
  *  The opponent will receive this with lastpos unused.
  *
  * ACTION_PLACE:
- *  This action can be received or returned. When received, lastpos indicates previously
- *  position placed on; when returned, newpos must be set to the position to place on.
+ *  This action can be received or returned. When received, lastpos
+ *  indicates previous position placed on; when returned, newpos must
+ *  be set to the position to place on.
  *
  * ACTION_UNPLACE:
- *  This action can be received or returned. When received, an unplacement by the opponent
- *  is indicated, with lastpos and newpos unused and return value ignored; when returned,
- *  the recent turn is undone with newpos unused, or it is ignored if no undo can be performed.
+ *  This action can be received or returned. When received, an
+ *  unplacement by the opponent is indicated, with lastpos and newpos
+ *  unused and return value ignored; when returned, the recent turn is
+ *  undone with newpos unused, or it is ignored if no undo can be
+ *  performed.
  *
  */
 
