1210c84306f6725b025ed41893a17306d63695dc
imhcyx <3443722+imhcyx@users.noreply.github.com>
2017-12-09 14:45:09 +0800

    impl of input

--- cli.c
+++ cli.c
@@ -8,6 +8,8 @@
 
 #define ROLENAME(x) (x == ROLE_BLACK ? "black" : "white")
 
+static pos newest[2];
+
 /* display the board */
 static void cli_display(board_t board) {
   int i, j;
@@ -20,10 +22,16 @@ static void cli_display(board_t board) {
     for (j=0; j<BOARD_W; j++) {
       switch (board[j][i]) {
       case 1:
-        printf("─●");
+        if (newest[0].x == j && newest[0].y == i)
+          printf("-▲");
+        else
+          printf("─●");
         break;
       case 2:
-        printf("─○");
+        if (newest[1].x == j && newest[1].y == i)
+          printf("-△");
+        else
+          printf("─○");
         break;
       default:
         printf("─┼");
@@ -42,6 +50,44 @@ static void cli_display(board_t board) {
   printf("\n");
 }
 
+/* 0 on failure, non-0 on success  */
+static int cli_parse_coordinate(char *str, pos *p) {
+  int i, j;
+  if (strlen(str) == 3) {
+    if (isalpha(str[0]) &&
+        isdigit(str[1]) &&
+        isdigit(str[2])) {
+      i = str[0] - (isupper(str[0])?'A':'a');
+      j = 10 * (str[1]-'0') + (str[2]-'0');
+    }
+    else if (isdigit(str[0]) &&
+        isdigit(str[1]) &&
+        isalpha(str[2])) {
+      i = str[2] - (isupper(str[2])?'A':'a');
+      j = 10 * (str[0]-'0') + (str[1]-'0');
+    }
+    else
+      return 0;
+  }
+  else if (strlen(str) == 2) {
+    if (isalpha(str[0]) && isdigit(str[1])) {
+      i = str[0] - (isupper(str[0])?'A':'a');
+      j = str[1] - '0';
+    }
+    else if (isdigit(str[0]) && isalpha(str[1])) {
+      i = str[1] - (isupper(str[1])?'A':'a');
+      j = str[0] - '0';
+    }
+    else
+      return 0;
+  }
+  else
+    return 0;
+  p->x = i;
+  p->y = BOARD_H - j;
+  return 1;
+}
+
 static int cli_callback(
     int role,
     int action,
@@ -54,6 +100,7 @@ static int cli_callback(
 {
   int i, result;
   char buf[128];
+  char *pmsg = 0;
 
   if (action == ACTION_NONE || action == ACTION_PLACE) {
 
@@ -64,37 +111,48 @@ static int cli_callback(
       //system("clear");
       printf("\n");
       cli_display(board);
-      printf("\n");
+      if (pmsg)
+        printf("%s\n", pmsg);
+      else
+        printf("\n");
 
       /* prompt for input */
       printf("%s>", ROLENAME(role));
       fgets(buf, sizeof(buf), stdin);
+      buf[strlen(buf)-1] = '\0';
 
       /* TODO: commands and EOF  */
 
-      /* try to recognize input as coordinates */
-      if (buf[0] >= 'A' &&
-          buf[0] < 'A' + BOARD_W &&
-          isdigit(buf[1]) &&
-          (i = atoi(&buf[1])) > 0 &&
-          i <= BOARD_H) {
+      /* handle quit  */
+      if (!strcmp(buf, "quit")) {
+        result = ACTION_GIVEUP;
+        break;
+      }
 
-        newpos->x = buf[0] - 'A';
-        newpos->y = BOARD_H - i; 
-        //printf("%d,%d\n", newpos->x, newpos->y);
- 
+      /* try to recognize input as coordinates */
+      if (cli_parse_coordinate(buf, newpos)) {
+
+        printf("%d,%d\n", newpos->x, newpos->y);
+
+        /* check validity */
+        if (newpos->x < 0 || newpos->x >= BOARD_W ||
+            newpos->y < 0 || newpos->y >= BOARD_H) {
+          pmsg = "invalid coordinate";
+        }
         /* check if occupied*/
-        if (board[newpos->x][newpos->y] == I_FREE) {
+        else if (board[newpos->x][newpos->y] == I_FREE) {
           result = ACTION_PLACE;
+          newest[role].x = newpos->x;
+          newest[role].y = newpos->y;
           break;
         }
         else {
-          printf("already occupied\n");
+          pmsg = "already occupied";
         }
 
       }
       else {
-        printf("unrecognized command\n");
+        pmsg = "unrecognized command";
       }
 
     }
--- main.c
+++ main.c
@@ -7,8 +7,17 @@
 #include "pai.h"
 
 int main(int argc, const char *argv[]) {
+  int winner;
   cli_register_player(ROLE_BLACK);
   cli_register_player(ROLE_WHITE);
-  printf("%d\n", pai_start_game());
+  winner = pai_start_game();
+  switch (winner) {
+  case ROLE_BLACK:
+    printf("black win\n");
+    break;
+  case ROLE_WHITE:
+    printf("white win\n");
+    break;
+  }
   return 0;
 }
